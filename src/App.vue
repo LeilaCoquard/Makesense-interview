c1912f9bd62a843f4b1b4173d29c55d16b8ada50
Leïla Coquard <leilacoquard@MacBook-Pro-de-Nassim.local>
2019-11-13 17:01:04 +0100

    resultats makesense challenge

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,83 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="label">Tags</div>
+    <div class="description" v-if="selectedTags.length === 0">
+      Sélectionne 2 tags dans la liste
+    </div>
+    <div class="description" v-if="selectedTags.length === 1">
+      Sélectionne encore 1 tag dans la liste
+    </div>
+    <div class="description" v-else>
+      Vous devez enlever un tag sélectionné pour en choisir un autre
+    </div>
+    <tagList
+      label="tags"
+      maxSelected="2"
+      :tags="listFromApi"
+      v-model="selectedTags"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import tagList from "./components/tagList.vue";
+import axios from "axios";
 
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      listFromApi: [],
+      selectedTags: ["5d41514a8f4abb59988c3245"]
+    };
+  },
   components: {
-    HelloWorld
+    tagList
+  },
+  mounted() {
+    axios
+      .get(
+        "https://api.projects.makesense.org/v1/taxonomies?limit=200&sort=-createdAt&type=legalForm"
+      )
+      .then(response => {
+        this.listFromApi = response.data.data;
+      });
   }
-}
+};
 </script>
 
 <style>
+@font-face {
+  font-family: "Raleway";
+  font-style: normal;
+  font-display: swap;
+  src: local("Raleway Black"), local("Raleway-Black"),
+    url(https://fonts.gstatic.com/s/raleway/v14/1Ptrg8zYS_SKggPNwK4vWqhPANqczVsq4A.woff2)
+      format("woff2");
+  unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB,
+    U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  font-size: 16px;
+}
+.label {
+  font-family: "Raleway";
+  font-size: 1.25em;
+  color: #0c3944;
+  text-align: left;
+  font-weight: 900;
+}
+.description {
+  font-family: "Raleway";
+  font-size: 16px;
+  color: #0c3944;
+  text-align: left;
+  font-weight: 300;
 }
 </style>
---
+++ src/components/tagList.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div class="tag-selected-container">
+      <div class="selected-tag tag" v-for="id in value" :key="id">
+        {{ displaySelectedTag(id) }}
+        <img class="croix" src="../image/croix.svg" />
+      </div>
+    </div>
+
+    <div class="tag-container">
+      <div
+        class="tag"
+        v-for="tag in tags"
+        :key="tag.id"
+        @click="addTag(tag._id)"
+      >
+        <p>{{ tag.name.fr }}</p>
+        <img class="plus" src="../image/plus.svg" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tagList",
+  props: ["label", "maxSelected", "tags", "value"],
+  methods: {
+    displaySelectedTag(id) {
+      return this.tags.find(tag => tag._id === id).name.fr;
+    },
+    addTag(id) {
+      this.value.push(id);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.tag-selected-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+.tag {
+  border: 2px solid #979797;
+  border-radius: 1.6em;
+  padding: 0.8em 1.5em;
+  padding-right: 2em;
+  display: flex;
+}
+.selected-tag {
+  background: #fabe3c;
+  border-color: #fabe3c;
+}
+.tag-container {
+  border: 2px dashed #e8e8e8;
+  border-radius: 6px;
+  padding: 1.25em;
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 1em;
+}
+p {
+  margin: 0;
+}
+.plus,
+.croix {
+  margin: 0.2em;
+}
+</style>
